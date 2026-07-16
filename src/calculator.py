4c91cc0787e881806eab058e02c3b73ef41cc248
”korshunnv” <korshunnv@gmail.com>
2026-07-16 15:12:14 +0300

    fix(calculator.py, test_calculator.py): add substraction operation

--- src/calculator.py
+++ src/calculator.py
@@ -7,3 +7,7 @@ def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Деление на ноль невозможно")
     return a / b
+
+def sub(a: float, b: float) -> float:
+    """Возвращает разность двух чисел."""
+    return a - b
